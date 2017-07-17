9aa629d8d772e997843397a0e00108677474658a
hejunwei <hejunwei@bluestoneapp.com>
2017-07-17 16:52:25 +0800

    fix debugbar show in production

--- config/app.php
+++ config/app.php
@@ -169,8 +169,6 @@ return [
          * Package Service Providers...
          */
 
-        Barryvdh\Debugbar\ServiceProvider::class,
-
         /*
          * Application Service Providers...
          */
