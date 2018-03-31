7caa4ac741cce01c93b8e363b3186b4830f7c02d
hejunwei <hejunwei@ziggurat.cn>
2018-03-31 16:43:15 +0800

    fix post login

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::group([
     Route::group([
         'namespace' => 'Auth',
     ], function () {
-        Route::get('login', 'LoginController@login');
+        Route::post('login', 'LoginController@login');
         Route::post('logout', 'LoginController@logout');
         Route::post('register', 'RegisterController@register');
     });
